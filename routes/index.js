d28da8571bfc49133743ae6d203b98365f26afd8
Ally Brannon <allison.brannon@gmail.com>
2020-02-28 08:53:18 -0500

    update with index file

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express'), 
-    router = express.Router();
+const express = require("express"),
+  router = express.Router();
 
 router.get(`/`, (req, res) => {
-    const snippet = '<h1>Hello World</h1>';
-    res.render(`template`, {
-        locals: {
-            title: 'Hello World'
-        },
-        partials: {
-            partial: 'partial-index'
-        }
-    });
+  const snippet = "<h1>Restaurant Info</h1>";
+  res.render(`template`, {
+    locals: {
+      title: "Restaurant"
+    },
+    partials: {
+      partial: "partial-index"
+    }
+  });
 });
 
 module.exports = router;
